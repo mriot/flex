2114d63f6461183a086dc2efddcf81348197ca60
Markus Kremer <mkremer@seibert-media.net>
2021-03-23 21:15:26 +0100

    relocate port creation etc

--- addon/src/background/Connection.js
+++ addon/src/background/Connection.js
@@ -15,25 +15,27 @@ class Connection {
 
   init() {
     return new Promise((resolve, reject) => {
+      // todo: get adress from settings
       this.ws = new WebSocket("ws://localhost:3210/dev");
-  
+      
       this.ws.onopen = (event) => {
         console.log("✅ Socket connection to local dev server established");
+        this.port = chrome.tabs.connect(this.tabId, {name: "flex"});
         resolve(true);
       };
     
       this.ws.onclose = (event) => {
         console.log("ℹ️ Socket connection was closed");
+        this.port.disconnect();
       };
     
       this.ws.onerror = (event) => {
-        console.log("❌ Error with socket connection");  
+        console.log("❌ Error with socket connection");
         resolve(false);
       };
   
       this.ws.onmessage = (event) => {
         this.data = JSON.parse(event.data);
-        this.port = chrome.tabs.connect(this.tabId, {name: "flex"});
         this.port.postMessage(this.data);
       };
     })
