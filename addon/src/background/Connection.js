62d2b8eafec24815e4e9ea497cf8abc2e20693d2
Markus Kremer <mkremer@seibert-media.net>
2021-03-23 22:25:36 +0100

    use WSS url from settings

--- addon/src/background/Connection.js
+++ addon/src/background/Connection.js
@@ -15,29 +15,32 @@ class Connection {
 
   init() {
     return new Promise((resolve, reject) => {
-      // todo: get adress from settings
-      this.ws = new WebSocket("ws://localhost:3210/dev");
+      chrome.storage.sync.get(storage => {
+        const url = storage.wssUrl || "ws://localhost:3210/dev";
+        this.ws = new WebSocket(url);
+
+        this.ws.onopen = (event) => {
+          console.log("✅ Socket connection to local dev server established");
+          this.port = chrome.tabs.connect(this.tabId, {name: "flex"});
+          resolve(true);
+        };
       
-      this.ws.onopen = (event) => {
-        console.log("✅ Socket connection to local dev server established");
-        this.port = chrome.tabs.connect(this.tabId, {name: "flex"});
-        resolve(true);
-      };
+        this.ws.onclose = (event) => {
+          console.log("ℹ️ Socket connection was closed");
+          this.port?.disconnect();
+        };
+      
+        this.ws.onerror = (event) => {
+          console.log("❌ Error with socket connection");
+          chrome.browserAction.setBadgeText({text: "FAIL", tabId: this.tabId});
+          resolve(false);
+        };
     
-      this.ws.onclose = (event) => {
-        console.log("ℹ️ Socket connection was closed");
-        this.port.disconnect();
-      };
-    
-      this.ws.onerror = (event) => {
-        console.log("❌ Error with socket connection");
-        resolve(false);
-      };
-  
-      this.ws.onmessage = (event) => {
-        this.data = JSON.parse(event.data);
-        this.port.postMessage(this.data);
-      };
-    })
+        this.ws.onmessage = (event) => {
+          this.data = JSON.parse(event.data);
+          this.port.postMessage(this.data);
+        };
+      });
+    });
   }
 }
