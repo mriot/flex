0782d06980919033332ea630b70f58bcac9e53df
Markus Kremer <mkremer@seibert-media.net>
2021-03-25 00:15:15 +0100

    wrap in IIFE and add js support

--- addon/src/content/content.js
+++ addon/src/content/content.js
@@ -8,64 +8,90 @@ const config = {
 };
 */
 
-chrome.storage.sync.get(storage => {
-  const url = storage.wssUrl || "ws://localhost:3210/dev";
-  ws = new WebSocket(url);
-
-  ws.onopen = (event) => {
-    console.log("✅ Socket connection to local dev server established");
-  };
-
-  ws.onclose = (event) => {
-    console.log("ℹ️ Socket connection was closed");
-  };
-
-  ws.onerror = (event) => {
-    console.log("❌ Error with socket connection");
-    chrome.browserAction.setBadgeText({text: "FAIL", tabId: tabId});
-  };
-
-  ws.onmessage = (event) => {
-    data = JSON.parse(event.data);
-    console.log(data);
-    processRequest(data);
-  };
-});
-
-function processRequest(data) {
-  switch (data.type) {
-    case "css":
-      if (!document.querySelector("#local_stylesheet")) {
-        const styleTag = document.createElement("style");
-        styleTag.setAttribute("id", "local_stylesheet");
-        document.querySelector("head").appendChild(styleTag);
-      }
-
-      if (data.removeNodes?.length > 0) {
-        data.removeNodes.forEach(element => {
-          try {
-            const node = document.querySelector(element);
-            if (node && node.parentNode) node.parentNode.removeChild(node);
-          } catch (error) {
-            console.log("Could not remove element with selector", element, error);
-          }
-        });
-      }
-
-      document.querySelector("#local_stylesheet").innerHTML = data.code;
-      break;
+(function() {
+  // check wether content script is already injected
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    sendResponse("pong");
+  });
   
-    default:
-      console.log("lol bro wtf", data.type);
+  // ws connection to server
+  chrome.storage.sync.get(storage => {
+    const url = storage.wssUrl || "ws://localhost:3210/dev";
+    ws = new WebSocket(url);
+  
+    ws.onopen = (event) => {
+      console.log("✅ Socket connection to local dev server established");
+    };
+  
+    ws.onclose = (event) => {
+      console.log("ℹ️ Socket connection was closed");
+    };
+  
+    ws.onerror = (event) => {
+      console.log("❌ Error with socket connection");
+      chrome.browserAction.setBadgeText({text: "FAIL", tabId: tabId});
+    };
+  
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+      processRequest(data);
+    };
+  });
+  
+  function processRequest(data) {
+    switch (data.type) {
+      case "css":
+        if (!document.querySelector("#local_stylesheet")) {
+          const styleTag = document.createElement("style");
+          styleTag.setAttribute("id", "local_stylesheet");
+          document.querySelector("body").appendChild(styleTag);
+        }
+  
+        if (data.removeNodes?.length > 0) {
+          data.removeNodes.forEach(element => {
+            try {
+              const node = document.querySelector(element);
+              if (node && node.parentNode) node.parentNode.removeChild(node);
+            } catch (error) {
+              console.log("Could not remove element with selector", element, error);
+            }
+          });
+        }
+  
+        document.querySelector("#local_stylesheet").textContent = data.code;
+
+        break;
+    
+      case "js": 
+        if (document.querySelector("#local_script")) {
+          console.log("reloading...");
+          window.location.reload();
+          return;
+        }
+
+        const scriptTag = document.createElement("script");
+        scriptTag.setAttribute("id", "local_script");
+        scriptTag.textContent = data.code;
+        document.querySelector("body").appendChild(scriptTag);
+
+        break;
+
+      default:
+        console.log("lol bro wtf", data.type);
+    }
   }
+})();
+
+
+/*  * /
+// say goodbye
+window.onbeforeunload = function() {
+  chrome.runtime.sendMessage("test", function(response) {
+    console.log(response);
+  });
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log(request);
-  sendResponse("pong");
-});
-
-/*  */
 chrome.runtime.onConnect.addListener(port => {
   console.log("connected to extension", port);
 
